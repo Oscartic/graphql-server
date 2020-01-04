74cbd1d0d39c609fcbd70b0c55b81cf2906e3c33
OscarB <oscartic@gmail.com>
2020-01-04 20:35:54 -0300

    example mutation with db memory ram

--- index.js
+++ index.js
@@ -8,19 +8,35 @@ app.get('/', (req, res) => {
     res.send("app ready for action!");
 });
 
-// resolver 
-const root = {client: () => {
-    return {
-        "id": 464354545,
-        "name": "Oscar ",
-        "last_name": "Ballesteros",
-        "company": "ORB",
-        "emails": [
-            {email: "oscar@mail.com" },
-            {email: "oscar@company.com" }
-        ]
+class Client {
+    constructor(id, {name, last_name, company, email}){
+        this.id = id;
+        this.name = name; 
+        this.last_name = last_name;
+        this.company = company;
+        this.email = email;
     }
-}}
+}
+
+const clientsDB = {}
+
+// resolver 
+const root = {
+    client: () => {
+        return {
+            "id": 464354545,
+            "name": "Oscar ",
+            "last_name": "Ballesteros",
+            "company": "ORB",
+            "email":  "oscar@mail.com",
+        }
+    },
+    createClient: ({input}) => {
+        const id = require('crypto').randomBytes(10).toString('hex');
+        clientsDB[id] = input;
+        return new Client(id, input);
+    }
+}
 
 app.use('/graphql', graphqlHTTP({
     // schema use for this url 
--- schema.js
+++ schema.js
@@ -2,17 +2,24 @@ import { buildSchema } from 'graphql';
 
 const schema = buildSchema(`
     type Client {
-        id: ID,
-        name: String,
-        last_name: String,
-        company: String,
-        emails: [Email] 
-    }
-    type Email {
-        email: String, 
+        id: ID
+        name: String
+        last_name: String
+        company: String
+        email: String
     }
     type Query {
         client: Client
+    }
+    input InputClient {
+        id: ID
+        name: String!
+        last_name: String!
+        company: String!
+        email: String!
+    }
+    type Mutation {
+        createClient(input: InputClient) : Client
     }
 `); 
 
